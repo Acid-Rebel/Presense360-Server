c3b47e49d91b239dd404fe5e0a78dd55af3c295a
Ajeth S <ajethsakthivel@gmail.com>
2025-10-28 22:18:54 +0530

    changed DB provider

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const { startAttendanceScheduler } = require('./attendance_scheduler'); 
 
-const connectionString =process.env.DATABASE_URL || "postgres://postgres:sql@123@localhost:5432/Presense360"
+const connectionString =process.env.DATABASE_URL || "postgres://avnadmin:AVNS_8wJSs4U-oUBVaIdhOXg@presense360-db-presense360-db.c.aivencloud.com:19049/defaultdb?sslmode=require"
 const client = new Client({
   connectionString, 
   ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
